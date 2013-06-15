1b5dc5d50f39dda8dd0341d9e623c63d31cd7e8f
Alexander Fabisch <afabisch@tzi.de>
2013-06-15 16:00:31 +0200

    Generate demonstration.

--- examples/two_dimensional.py
+++ examples/two_dimensional.py
@@ -1,18 +1,36 @@
 from pyseds.seds import SEDS
 import numpy
+import pylab
 
 
-def generate_demonstrations(n_demos, n_task_dims, n_steps):
-    S = numpy.ndarray((n_demos, n_task_dims, n_steps))
+def generate_demonstrations():
+    n_demos = 1
+    n_task_dims = 2
+    n_steps = 100
+    delta_t = 0.01
+
     Sd = numpy.ndarray((n_demos, n_task_dims, n_steps))
+    S = numpy.ndarray((n_demos, n_task_dims, n_steps))
     for d in range(n_demos):
-        pass
-        # TODO
+        a = numpy.zeros(n_task_dims)
+        v = numpy.zeros(n_task_dims)
+        s = numpy.zeros(n_task_dims)
+        for t in range(0, n_steps):
+            s += v*delta_t
+            v += a*delta_t
+            a = numpy.array([10.0*(t/(n_steps-2.0)-0.5)**3,
+                             numpy.sin(2*numpy.pi*t/(n_steps-2.0))])
+            Sd[d, :, t] = v
+            S[d, :, t] = s
+            print a, v, s
+
     return S, Sd
 
 
 if __name__ == "__main__":
-    S, Sd = generate_demonstrations(2, 2, 10)
+    S, Sd = generate_demonstrations()
+    pylab.plot(S[0, 0, :], S[0, 1, :])
+    pylab.show()
 
     seds = SEDS(2)
     seds.imitate(S, Sd)

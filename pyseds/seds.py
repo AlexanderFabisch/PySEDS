0d2d3aaf4f78c2445523742b0b73d28fe4513628
Alexander Fabisch <afabisch@tzi.de>
2013-06-15 20:41:42 +0200

    Prepare minimization.

--- pyseds/seds.py
+++ pyseds/seds.py
@@ -1,6 +1,17 @@
 from pyseds.gm import GaussianMixture
 import numpy
+import scipy.optimize
 from sklearn.mixture import GMM
+
+
+def _to_vector(weights, means, covars):
+    return numpy.hstack((weights.flatten(), means.flatten(), covars.flatten()))
+
+
+def _from_vector(x, weights, means, covars):
+    weights = x[:weights.size]
+    means = x[weights.size:-covars.size].reshape(means.shape)
+    covars = x[-covars.size:].reshape(covars.shape)
 
 
 class SEDS(object):
@@ -11,7 +22,14 @@ class SEDS(object):
 
     def imitate(self, S, Sd):
         weights, means, covars = self.__initial_parameters(S, Sd)
-        # TODO optimize with scipy.optimize.fmin_slsqp
+
+        # TODO conditions
+        cost = lambda x: self.__cost(x)
+        cost_deriv = lambda x: self.__cost_deriv(x)
+        x0 = _to_vector(weights, means, covars)
+        scipy.optimize.fmin_slsqp(func=cost, fprime=cost_deriv, x0=x0,
+                                  eqcons=[], ieqcons=[])
+
         self.gm = GaussianMixture(weights, means, covars)
 
     def __initial_parameters(self, S, Sd):
@@ -45,6 +63,14 @@ class SEDS(object):
                 means[k, :n_task_dims] - self.attractor))
 
         return weights, means, covars
+
+    def __cost(self, x):
+        # TODO log-likelihood
+        return 0.0
+
+    def __cost_deriv(self, x):
+        # TODO derivatives
+        return None
 
     def step(self, s):
         """Compute next desired velocity from current state.

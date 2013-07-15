7caf60a18927b121fa5e655798c1213320d0fae1
Alexander Fabisch <Alexander.Fabisch@dfki.de>
2013-07-15 16:57:22 +0200

    (Un)pack parameters.

--- pyseds/seds.py
+++ pyseds/seds.py
@@ -4,23 +4,26 @@ import scipy.optimize
 from sklearn.mixture import GMM
 
 
-def _to_vector(weights, means, covars):
-    # TODO cholesky
-    return numpy.hstack((numpy.log(weights).flatten(), means.flatten(), covars.flatten()))
+def _to_optimization_vector(weights, means, covars):
+    chol_decomps = numpy.array([numpy.linalg.cholesky(covar) for covar in covars])
+    return numpy.hstack((numpy.log(weights), means.flatten(), chol_decomps.flatten()))
 
 
 def _from_optimization_vector(theta, weights, means, covars):
-    # TODO
-    weights = theta[:weights.size]
-    means = theta[weights.size:-covars.size].reshape(means.shape)
-    covars = theta[-covars.size:].reshape(covars.shape)
+    assert theta.size == (weights.size + means.size + covars.size)
+    weights[:] = numpy.exp(theta[:weights.size])
+    means[:,:] = theta[weights.size:-covars.size].reshape(means.shape)
+    covars[:, :, :] = numpy.array([chol_decomp.dot(chol_decomp.T)
+                                   for chol_decomp in theta[-covars.size:]
+                                   .reshape(covars.shape)])
 
 
 class SEDS(object):
-    def __init__(self, attractor, n_components):
+    def __init__(self, attractor, n_components, debug=True):
         self.attractor = numpy.asarray(attractor)
         self.n_components = n_components
         self.n_task_dims = len(attractor)
+        self.debug = debug
 
     def imitate(self, S, Sd):
         weights, means, covars = self.__initial_parameters(S, Sd)
@@ -32,12 +35,26 @@ class SEDS(object):
         # mu1, ..., muK are the means and L1, ..., LK are the Cholesky
         # decompositions of the covariance matrices (these do always exist
         # since covariance matrices are positive definite).
-        cost = lambda theta: self.__cost(theta)
-        cost_deriv = lambda theta: self.__cost_deriv(theta)
         theta0 = _to_optimization_vector(weights, means, covars)
+
+        if self.debug:
+            # Test packing / unpacking
+            w = numpy.copy(weights)
+            m = numpy.copy(means)
+            c = numpy.copy(covars)
+            _from_optimization_vector(theta0, w, m, c)
+            assert (w - weights < 1e-5).all()
+            assert (m - means < 1e-5).all()
+            assert (c - covars < 1e-5).all()
+
         # TODO conditions, cost function
-        #scipy.optimize.fmin_slsqp(func=self.cost, fprime=self.cost_deriv,
-        #                          theta0=theta0, eqcons=[], ieqcons=[])
+        def f(theta):
+            _from_optimization_vector(theta, weights, means, covars)
+            return self.__cost_deriv(weights, means, covars)[0]
+        def fp(theta):
+            _from_optimization_vector(theta, weights, means, covars)
+            return self.__cost_deriv(weights, means, covars)[1]
+        scipy.optimize.fmin_slsqp(f, fprime=fp, x0=theta0, eqcons=[], ieqcons=[])
 
         self.gm = GaussianMixture(weights, means, covars)
 
@@ -73,13 +90,12 @@ class SEDS(object):
 
         return weights, means, covars
 
-    def __cost(self, x):
-        # TODO log-likelihood
-        return 0.0
-
-    def __cost_deriv(self, x):
-        # TODO derivatives
-        return None
+    def __cost_deriv(self, weights, means, covars):
+        # TODO implement
+        weights_deriv = numpy.zeros_like(weights)
+        means_deriv = numpy.zeros_like(means)
+        covars_deriv = numpy.zeros_like(covars)
+        return 0.0, numpy.hstack((weights_deriv, means_deriv.flatten(), covars_deriv.flatten()))
 
     def step(self, s):
         """Compute next desired velocity from current state.
